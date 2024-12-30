ba8c0f915a580f029ad7a5586e3e20bf63bf6d3b
hamzarockville <hamza.hayat@rockvillegroup.com>
2024-12-30 15:28:58 +0500

    Last Fiz

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,22 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: ['https://quiz-app-quiz-react-app.eqpf0a.easypanel.host'], // Specify the frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
+  });
+
+  // Handle preflight OPTIONS requests
+  app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Origin', 'https://quiz-app-quiz-react-app.eqpf0a.easypanel.host');
+      res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+      res.status(204).send();
+    } else {
+      next();
+    }
   });
   app.useGlobalPipes(new ValidationPipe());
   app.getHttpAdapter().getInstance().setTimeout(300000);
